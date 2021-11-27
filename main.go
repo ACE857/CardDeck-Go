d6745226c73a026f2467b65d33cdacde33ced03c
ace857 <srjbsht857@gmail.com>
2021-11-27 21:25:31 +0530

    Added deck.go with custom deck type and added print reciver to it.

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	// Go has 2 DS for handling list of records - array(fixed length), slice(dynamic length)
-	cards := []string{"Ace of diamonds", newCard()}
+	cards := deck{"Ace of diamonds", newCard()}
 	cards = append(cards, "Six of spades") // appends does not moodify existing slice it returns a new one
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
