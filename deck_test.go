2a2de9d220716c27136dbfffad6cf0ecafd8ec1e
ace857 <srjbsht857@gmail.com>
2021-12-04 21:36:05 +0530

    added test cases for newCards

--- deck_test.go
+++ deck_test.go
@@ -7,4 +7,10 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Deck length invalid expected 52 but got $%v", len(d))
 	}
+	if d[0] != "Ace of Heart" {
+		t.Errorf("Deck card[0] invalid expected 'Ace of Heart' but got $%v", d[0])
+	}
+	if d[len(d)-1] != "Ten of Club" {
+		t.Errorf("Deck card[51] invalid expected 'Ten of Club' but got $%v", d[len(d)-1])
+	}
 }
